41f403fb1501c4667f9d3f7c0e8f668a02910743
balazs987 <bpataki1984@gmail.com>
2020-05-19 14:30:42 +0200

    header, hero, about, error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="about-section section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente,
+        adipisci vel omnis ipsa molestias nihil quas. Deserunt inventore
+        doloribus nisi tempora aliquid dolorem tempore cum eos nemo? Voluptatem
+        debitis assumenda deleniti, suscipit quibusdam corrupti at veniam facere
+        consequuntur dolorem error qui enim incidunt magni eum porro, nulla,
+        fugit nihil.
+      </p>
+    </section>
+  );
 }
